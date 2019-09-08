d06e65f526d917791280c38b98a8feaba32135a4
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-09-08 15:29:37 +0900

    End odf section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-vents</h1>
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </React.Fragment>
     );
   }
 }
