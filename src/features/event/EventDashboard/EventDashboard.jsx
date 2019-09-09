a39e4cd3105606222385b9db7a9762386982752b
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-09-09 18:18:37 +0900

    End of Section4

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid'
 
 const eventsFromDashboard = [
   {
@@ -78,6 +79,19 @@ class EventDashboard extends Component {
     ))
   }
 
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    this.setState(({events}) => ( 
+      //{}で括るとstate内にアクセスができる。なので{events}はstate内のevents
+      {
+        events: [...events, newEvent],
+        isOpen: false
+      }
+    ))
+
+  }
+
   render() {
     const { events, isOpen } = this.state
     return (
@@ -87,7 +101,7 @@ class EventDashboard extends Component {
         </Grid.Column>
         <Grid.Column width={6}>
           <Button onClick={this.handleIsOpenToggle} positive content='Create Event'/>
-          {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle} />}
+          {isOpen && <EventForm createEvent={this.handleCreateEvent} cancelFormOpen={this.handleIsOpenToggle} />}
         </Grid.Column>
       </Grid>
     )
